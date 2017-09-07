38c15ff67afbfa69fa25b32c53ed1684a97c82a2
Gianluca Fattarsi <fattazzo82@gmail.com>
2017-09-07 12:41:58 +0200

    Detail drivers test updated

--- app/src/androidTest/java/com/gmail/fattazzo/formula1world/fragments/current/drivers/detail/DetailDriverFragmentTest.java
+++ app/src/androidTest/java/com/gmail/fattazzo/formula1world/fragments/current/drivers/detail/DetailDriverFragmentTest.java
@@ -1,5 +1,6 @@
 package com.gmail.fattazzo.formula1world.fragments.current.drivers.detail;
 
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.contrib.NavigationViewActions;
 import android.view.Gravity;
 
@@ -9,12 +10,15 @@ import com.gmail.fattazzo.formula1world.domain.F1Driver;
 import com.gmail.fattazzo.formula1world.domain.F1Result;
 import com.gmail.fattazzo.formula1world.ergast.Ergast;
 import com.gmail.fattazzo.formula1world.ergast.Ergast_;
+import com.gmail.fattazzo.formula1world.fragments.current.drivers.comparator.DriverNameComparator;
 import com.gmail.fattazzo.formula1world.service.DataService;
 import com.gmail.fattazzo.formula1world.service.DataService_;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import static android.support.test.espresso.Espresso.onData;
@@ -26,6 +30,7 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.DrawerActions.open;
 import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.gmail.fattazzo.formula1world.matchers.Matchers.withChildViewCount;
@@ -40,14 +45,9 @@ import static org.hamcrest.Matchers.anything;
 public class DetailDriverFragmentTest extends BaseTest {
 
     @Test
-    public void test2016First5Progress() {
-        Ergast ergast = Ergast_.getInstance_(getContext());
-        ergast.setSeason(2016);
-        DataService dataService = DataService_.getInstance_(getContext());
-        List<F1Driver> drivers = dataService.loadDrivers();
-
-        for (int i=0; i<=5; i++) {
-
+    public void test2016Progress() {
+        for (int i=2010; i<=2016; i++) {
+            testSeasonProgress(i);
         }
     }
 
@@ -72,7 +72,7 @@ public class DetailDriverFragmentTest extends BaseTest {
         onView(withId(R.id.title)).check(matches(withText("Sebastian Vettel")));
 
         // Progress
-        onView(withId(R.id.view_pager)).check(matches(hasDescendant(withText("Progress"))));
+        onView(withId(R.id.view_pager)).check(matches(hasDescendant(withText(getContext().getString(R.string.detail_driver_season_progress_tab_title)))));
         onView(withId(R.id.progress_driver_table_layout)).check(matches(withChildViewCount(vettelResults.size()+1, null)));
 
         int pos=0;
@@ -91,13 +91,46 @@ public class DetailDriverFragmentTest extends BaseTest {
 
         // Classifica
         onView(withId(R.id.view_pager)).perform(swipeLeft());
-        onView(withId(R.id.view_pager)).check(matches(hasDescendant(withText("Classifica"))));
+        onView(withId(R.id.view_pager)).check(matches(hasDescendant(withText(getContext().getString(R.string.detail_driver_ranking_tab_title)))));
 
         // Info
         onView(withId(R.id.view_pager)).perform(swipeLeft());
-        onView(withId(R.id.view_pager)).check(matches(hasDescendant(withText("Info"))));
+        onView(withId(R.id.view_pager)).check(matches(hasDescendant(withText(getContext().getString(R.string.info_fragment_title)))));
 
         onView(withId(R.id.view_pager)).perform(swipeRight());
         onView(withId(R.id.view_pager)).perform(swipeRight());
+    }
+
+    private void testSeasonProgress(int season) {
+        selectSeason(season);
+
+        onView(withId(R.id.drawer_layout)).check(matches(isClosed(Gravity.START))).perform(open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_current_season_driver));
+
+        Ergast ergast = Ergast_.getInstance_(getContext());
+        ergast.setSeason(season);
+        DataService dataService = DataService_.getInstance_(getContext());
+        List<F1Driver> drivers = dataService.loadDrivers();
+        Collections.sort(drivers,new DriverNameComparator());
+
+        for (int i=0; i<=drivers.size()-1; i++) {
+            onData(anything()).inAdapterView(withId(R.id.list_view)).atPosition(i).onChildView(withId(R.id.driver_item_name)).perform(click());
+
+            List<F1Result> driverResults = dataService.loadDriverRacesResult(drivers.get(i));
+            int pos=0;
+            for (F1Result result:driverResults) {
+                onView(withIndex(withId(R.id.race_results_position), pos)).check(matches(withText(result.positionText)));
+                onView(withIndex(withId(R.id.race_results_laps), pos)).check(matches(withText(String.valueOf(result.laps))));
+                onView(withIndex(withId(R.id.race_results_grid), pos)).check(matches(withText(String.valueOf(result.grid))));
+                String time = "-";
+                if (result.time != null) {
+                    time = StringUtils.defaultString(result.time.time,"-");
+                }
+                onView(withIndex(withId(R.id.race_results_time), pos)).check(matches(withText(time)));
+                onView(withIndex(withId(R.id.race_results_status), pos)).check(matches(withText(result.status)));
+                pos++;
+            }
+            onView(isRoot()).perform(ViewActions.pressBack());
+        }
     }
 }
